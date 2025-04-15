682f8564c12d6f3bf898c503b684a1be22ad9556
ydv-prdp <pradeepyadav101010@gmail.com>
2025-04-15 22:35:33 +0530

    header comp added

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,8 @@
 import type { Metadata } from "next";
 import { Source_Sans_3 as FontSans } from "next/font/google";
 import "./globals.css";
+import Header from "@/components/common/header";
+import Footer from "@/components/common/footer";
 
 const fontSans = FontSans({
   variable: "--font-sans",
@@ -25,7 +27,13 @@ export default function RootLayout({
       <body
         className={`${fontSans.variable} font-sans antialiased`}
       >
-        {children}
+        <div className="flex min-h-screen flex-col">
+          <Header/>
+          <main className="flex-1">
+            {children}
+          </main>
+          <Footer/>
+        </div>
       </body>
     </html>
   );
---
+++ components/common/header.tsx
@@ -0,0 +1,41 @@
+import { FileText } from "lucide-react"
+import NavLink from "./nav-link";
+
+const Header = () => {
+    const isLoggedIn = false;
+  return (
+    <nav className="container flex  items-center justify-between py-4 lg:px-8 px-2 mx-auto">
+        <div className="flex lg:flex-1 gap-1 items-center">
+             <NavLink href="/" className="flex items-center gap-1 lg:gap-2 shrink-0">
+                <FileText className="w-5 h-5 lg:w-8 lg:h-8 text-gray-900 hover:rotate-12 transform transition duration-200 ease-in-out"/>
+                <span className="font-extrabold lg:text-xl text-gray-900">
+                    Summarizer
+                </span>
+                
+            </NavLink>
+        </div>
+        <div className="flex lg:justify-center gap-4  lg:gap-12 lg:items-center">
+            <NavLink href="/pricing">
+                Pricing
+            </NavLink>
+            {isLoggedIn && <NavLink href="/dashboard">
+                Your Summaries
+            </NavLink>}
+        </div>
+        <div className="flex lg:justify-end lg:flex-1">
+            {isLoggedIn ? 
+                <div className="flex gap-2 items-center">
+                    <NavLink href={"/upload"}>
+                        Upload a PDF
+                    </NavLink>
+                </div> : 
+                <NavLink href="/sign-in">
+                    Sign In
+                </NavLink>
+            }
+        </div>
+    </nav>
+  )
+}
+
+export default Header
