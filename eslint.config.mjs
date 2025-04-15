9ba7e136e430cf002e5d082b7139b6be359ecea0
ydv-prdp <pradeepyadav101010@gmail.com>
2025-04-15 19:40:28 +0530

    added basic dependecies and packages

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -10,7 +10,15 @@ const compat = new FlatCompat({
 });
 
 const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
+  ...compat.config({
+    extends: ['next','prettier'],
+    plugins:['prettier'],
+    rules: {
+      'react/no-unescaped-entities': 'off',
+      '@next/next/no-page-custom-font': 'off',
+    },
+  }),
 ];
+
 
 export default eslintConfig;
